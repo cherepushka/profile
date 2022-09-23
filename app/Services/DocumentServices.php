0f5c5ec07218b3a2209a343e85af6e7635ca29c3
lavreek <lavreeksolacki@yandex.ru>
2022-09-23 16:43:36 +0300

    Services update

--- app/Services/DocumentServices.php
+++ app/Services/DocumentServices.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Enums\Section;
 use App\Models\Document;
+use Exception;
 use Illuminate\Support\Facades\Storage;
 
 class DocumentServices
@@ -13,9 +14,23 @@ class DocumentServices
         return $this;
     }
 
+    /**
+     * Обработка информации о документах
+     *
+     * @param Document $document - Экземпляр модели Document из модели Invoice.
+     * Прошедший предварительную валидацию и обработку методом map()
+     * @param string $file - Файл преобразованный в base64 строку
+     * @param Section $section - Перечисление секций загрузки
+     * @param string $user_password - Хэш, пароль пользователя
+     * @param string $archive_password - Пароль от архива
+     * @return void
+     */
     public function getData(Document $document, string $file, Section $section, string $user_password,
                             string $archive_password)
     {
+        /**
+         * Путь к активной директории
+         */
         $storage = storage_path("app/documents/orders/{$document->order_id}/");
 
         /**
@@ -53,13 +68,23 @@ class DocumentServices
         $this->encrypt($storage, $document, $user_password);
     }
 
-    private function dbSaveDoc(Document $doc, Section $section)
+    /**
+     * Сохранение полученного из данных файла в базу данных
+     *
+     * @param Document $doc
+     * @param Section $section
+     * @return void
+     */
+    private function dbSaveDoc(Document $doc, Section $section): void
     {
         $doc->section = $section->getSection();
 
         $fileinfo = pathinfo($doc->filename);
         $doc->extension = $fileinfo['extension'];
-        $doc->filename =  str_replace(['.', " "], '', $fileinfo['filename']).".".$fileinfo['extension'];
+        /**
+         * Требует дополнительной проверки
+         */
+        //$doc->filename =  str_replace(['.', " "], '', $fileinfo['filename']).".".$fileinfo['extension'];
 
         Document::updateOrCreate(
             ['filename' => $doc->filename, 'section' => $doc->section],
@@ -73,14 +98,29 @@ class DocumentServices
         );
     }
 
-    private function saveInvoiceFile(Document $doc, string $fileBase64)
+    /**
+     * Сохранение файла с полученных данных
+     *
+     * @param Document $doc
+     * @param string $fileBase64
+     * @return void
+     */
+    private function saveInvoiceFile(Document $doc, string $fileBase64): void
     {
         $storage = Storage::disk('orders');
         $storage->makeDirectory($doc->order_id);
         $storage->put("/{$doc->order_id}/".$doc->filename, base64_decode($fileBase64));
     }
 
-    private function pack($storage, $section, $filename)
+    /**
+     * Создание нового архива
+     *
+     * @param $storage
+     * @param $section
+     * @param $filename
+     * @return void
+     */
+    private function pack($storage, $section, $filename): void
     {
         unlink($storage.$filename);
         $zip = new \ZipArchive;
@@ -101,11 +141,19 @@ class DocumentServices
             }
             $zip->close();
         } else {
-            return response()->json("Failed opening archive: ". @$zip->getStatusString() . " (code: ". $zip_status .")");
+            response()->json("Failed opening archive: " . @$zip->getStatusString() . " (code: " . $zip_status . ")");
         }
     }
 
-    private function unpack($storage, $document, $password)
+    /**
+     * Распаковка архива
+     *
+     * @param string $storage
+     * @param Document $document
+     * @param string $password
+     * @return void
+     */
+    private function unpack(string $storage, Document $document, string $password): void
     {
         $file = $storage.$document->filename;
         $info = pathinfo($file);
@@ -117,18 +165,28 @@ class DocumentServices
             if ($zip_status === true) {
                 if ($zip->setPassword($password)) {
                     if (!$zip->extractTo($storage)) {
-                        return response()->json("Extraction failed (wrong password?)");
+                        response()->json("Extraction failed (wrong password?)");
+                        return;
                     }
                 }
 
                 $zip->close();
             } else {
-                return response()->json("Failed opening archive: ". @$zip->getStatusString() . " (code: ". $zip_status .")");
+                response()->json("Failed opening archive: " . @$zip->getStatusString() . " (code: " . $zip_status . ")");
             }
         }
     }
 
-    private function encrypt($storage, $document, $user_password, $filename = "")
+    /**
+     * Шифрование openssl файлов и каталогов.
+     *
+     * @param $storage - Путь до директории
+     * @param $document - Модель документа
+     * @param $user_password - Хэш пароль пользователя
+     * @param string $filename - Обязательный параметр, при кодировки архива или заданного файла.
+     * @return void
+     */
+    private function encrypt(string $storage, Document $document, string $user_password, string $filename = ""): void
     {
         $files = scandir($storage);
 
@@ -144,29 +202,53 @@ class DocumentServices
                         $iv = openssl_random_pseudo_bytes($ivlen);
 
                         $ciphertext = @openssl_encrypt($content, 'AES-256-CBC', $user_password, true, $iv);
-                        file_put_contents($storage.$value, $iv.$ciphertext);
+                        file_put_contents($storage.$value, $iv.$ciphertext); // Не забыть удалять 16 байт в скаченном файле
                     }
                 }
             }
         } else {
             $content = file_get_contents($storage.$filename);
 
-            $ivlen = openssl_cipher_iv_length($cipher="AES-256-CTR");
+            $ivlen = openssl_cipher_iv_length($cipher="AES-256-CBC");
             $iv = openssl_random_pseudo_bytes($ivlen);
 
-            $ciphertext = @openssl_encrypt($content, 'AES-256-CTR', $user_password, true, $iv);
-            file_put_contents($storage.$filename, $iv.$ciphertext);
+            $ciphertext = @openssl_encrypt($content, 'AES-256-CBC', $user_password, true, $iv);
+            file_put_contents($storage.$filename, $iv.$ciphertext); // Не забыть удалять 16 байт в скаченном файле
         }
     }
-    private function decrypt()
+
+    /**
+     * Расшифрока файлов и каталогов
+     *
+     * @return never
+     * @throws Exception
+     * @deprecated
+     */
+    private function decrypt(): void
     {
-//        $file = file_get_contents("test");
-//        $key = "123";
-//        $ciphertext = openssl_decrypt($file, 'AES-256-CTR', $key, true);
-//        file_put_contents("decrypt.zip", $ciphertext);
+        /* Последовательность расшифровки посредством языка PHP
+
+            $content - @string / Получить архив
+            $iv = @string / Векторное смещение => fread($сcntent, 16); / Считать первые 16 байт
+            $key = @string / Пароль пользователя;
+            $zip = fopen("here.zip", "r");
+            $ciphertext = openssl_decrypt(
+                stream_get_contents($file), / Обрезание первых символов
+                'AES-256-CBC', / Метод кодировки
+                $key, true, $iv
+            );
+        */
+        throw new Exception('Deprecated function not allowed to execute.', 404);
     }
 
-    private function scanNewFiles(Document $parent, $storage)
+    /**
+     * Просмотр директории после распаковки и добавление файлов в базу данных
+     *
+     * @param Document $parent
+     * @param $storage
+     * @return void
+     */
+    private function scanNewFiles(Document $parent, $storage): void
     {
         $files = array_diff(scandir($storage), [".", ".."]);
 
